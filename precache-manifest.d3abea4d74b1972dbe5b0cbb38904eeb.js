1751bb9755f8da68f26363085d915553a8a182a7
Steel <https://github.com/Steelmen87/>
2022-07-11 18:01:23 +0300

    Updates

---
+++ precache-manifest.d3abea4d74b1972dbe5b0cbb38904eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c9f23a2ffd34248d5f6669dfcd1864",
+    "url": "/Todo-list-it-incubator/index.html"
+  },
+  {
+    "revision": "aba6eaa94ac670107e1f",
+    "url": "/Todo-list-it-incubator/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "f543ebb13449f015479b",
+    "url": "/Todo-list-it-incubator/static/js/2.c4a9203b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todo-list-it-incubator/static/js/2.c4a9203b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba6eaa94ac670107e1f",
+    "url": "/Todo-list-it-incubator/static/js/main.44e6745b.chunk.js"
+  },
+  {
+    "revision": "9ef7265b07a7703e25ec",
+    "url": "/Todo-list-it-incubator/static/js/runtime-main.3750f1d7.js"
+  }
+]);
